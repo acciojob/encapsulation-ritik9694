05e3e3a1b2126fb629072466e368369f269c3c62
ritik9694 <ritikkumarbhai@gmail.com>
2024-11-26 09:36:58 +0530

    encapsulation assignment completed

--- src/main/java/com/driver/Main.java
+++ src/main/java/com/driver/Main.java
@@ -1,5 +1,14 @@
 package com.driver;
 
 public class Main {
-  
+    public static void main(String[] args) {
+        RWOnly rwOnly = new RWOnly();
+
+        //Try setting a value to name by directly accessing it using obj and also try printing it
+//        rwOnly.name = "hrithik";
+        //•name' has private access in 'com.driver.RWOnly•
+
+        rwOnly.setName("hrithik");
+        System.out.println(rwOnly.getName());
+    }
 }
